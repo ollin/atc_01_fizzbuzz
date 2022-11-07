1897aa93903a4972911d8fa55a463ea4979cbd50
Oliver Nautsch <oliver@brixel.io>
2022-11-07 20:54:51 +0100

    e initial commit

---
+++ src/test/java/ch/zurich/eth/fizzbuzz/FizzBuzzShould.java
@@ -0,0 +1,13 @@
+package ch.zurich.eth.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FizzBuzzShould {
+
+    @Test
+    void dada() {
+        assertThat(true).isEqualTo(false);
+    }
+}
